51b60f0f0deafda3bca868be3003c04cbd415c01
Christopher Grant <crgrant@gmail.com>
2020-10-01 11:45:53 -0500

    Simple Golang Rest API

---
+++ golang-rest-todo/README.md
@@ -0,0 +1,3 @@
+# Golang Rest Todo
+
+This app represents a simple mock rest service. The service provides basic CRUD operations on an im-memory representation of a todo list.  
---
+++ golang-rest-todo/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Todo struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var todos []Todo
+
+func main() {
+	router := mux.NewRouter()
+	todos = append(todos, Todo{ID: "1", Title: "Mow the lawn", Body: "Make sure you mow the lawn before the party"})
+	todos = append(todos, Todo{ID: "2", Title: "Buy Milk", Body: "Don't forget the milk. Dry cereal is the worst"})
+
+	router.HandleFunc("/", hello).Methods("GET")
+	router.HandleFunc("/todos", getTodos).Methods("GET")
+	router.HandleFunc("/todos", createTodo).Methods("POST")
+	router.HandleFunc("/todos/{id}", getTodo).Methods("GET")
+	router.HandleFunc("/todos/{id}", updateTodo).Methods("PUT")
+	router.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
+
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range todos {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			break
+		}
+		return
+	}
+	json.NewEncoder(w).Encode(&Todo{})
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var todo Todo
+	_ = json.NewDecoder(r.Body).Decode(todo)
+	todo.ID = strconv.Itoa(rand.Intn(1000000))
+	todos = append(todos, todo)
+	json.NewEncoder(w).Encode(&todo)
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range todos {
+		if item.ID == params["id"] {
+			todos = append(todos[:index], todos[index+1:]...)
+			var todo Todo
+			_ = json.NewDecoder(r.Body).Decode(todo)
+			todo.ID = params["id"]
+			todos = append(todos, todo)
+			json.NewEncoder(w).Encode(&todo)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(todos)
+}
+
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range todos {
+		if item.ID == params["id"] {
+			todos = append(todos[:index], todos[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(todos)
+}
